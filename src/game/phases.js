e1edfd2b05b1ba312121b7788f3bceb654e76f34
Raclette2Fromage <79201172+Raclette2Fromage@users.noreply.github.com>
2025-08-21 15:32:07 +0200

    Create phases.js

---
+++ src/game/phases.js
@@ -0,0 +1,17 @@
+// Nuit 0 (pré-setup)
+export const NIGHT0_ORDER = ["cupidon", "enfant_sauvage"]; // Capitaine: élection séparée si activé
+
+
+// Nuit standard – ordre proposé
+export const NIGHT_ORDER = [
+"salvateur",
+"voyante",
+"voyante_bavarde",
+"detective",
+"petite_fille",
+"corbeau",
+"loups",
+"loup_blanc",
+"infect_pere",
+"sorciere"
+];
