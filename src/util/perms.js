6b6cbd9791f3b3b4fdcf5c0a0976022ce442e764
Raclette2Fromage <79201172+Raclette2Fromage@users.noreply.github.com>
2025-08-21 15:33:24 +0200

    Create perms.js

---
+++ src/util/perms.js
@@ -0,0 +1,6 @@
+import { PermissionsBitField } from "discord.js";
+export const VIEW_WRITE = [
+PermissionsBitField.Flags.ViewChannel,
+PermissionsBitField.Flags.SendMessages,
+PermissionsBitField.Flags.ReadMessageHistory
+];
